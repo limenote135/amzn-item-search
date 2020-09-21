bb44a6a37c1e2cd50a6114f95b664aeeeb696d63
limenote135 <17950108+limenote135@users.noreply.github.com>
2020-09-22 00:14:11 +0900

    settings の保持対応

--- lib/pages/search/search_settings_page/search_settings_page.dart
+++ lib/pages/search/search_settings_page/search_settings_page.dart
@@ -1,7 +1,12 @@
+import 'package:ama_search/controllers/search_settings_controller.dart';
+import 'package:ama_search/models/search_settings.dart';
 import 'package:ama_search/models/search_type.dart';
 import 'package:ama_search/models/used_sub_condition.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/all.dart';
+
+final _currentSettingsProvider = ScopedProvider<SearchSettings>(null);
 
 class SearchSettingsPage extends StatelessWidget {
   const SearchSettingsPage({Key key}) : super(key: key);
@@ -24,44 +29,59 @@ class _Body extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: ListTile.divideTiles(
-        context: context,
-        tiles: [
-          const _CodeType(),
-          const _UsedSubCondition(),
-          SwitchListTile(
-            value: true, // TODO: 仮
-            title: const Text("FBA 利用"),
-            onChanged: (value) {
-              //TODO: 変更対応
-            },
-          ),
-          SwitchListTile(
-            value: true, // TODO: 仮
-            title: const Text("FBA 出品を優先表示"),
-            onChanged: (value) {
-              //TODO: 変更対応
-            },
-          ),
-        ],
-      ).toList(),
+    final settings = useProvider(searchSettingsControllerProvider.state);
+    return ProviderScope(
+      overrides: [
+        _currentSettingsProvider.overrideWithValue(settings),
+      ],
+      child: ListView(
+        children: ListTile.divideTiles(
+          context: context,
+          tiles: [
+            const _CodeType(),
+            const _UsedSubCondition(),
+            SwitchListTile(
+              value: settings.useFba,
+              title: const Text("FBA 利用"),
+              onChanged: (value) {
+                if (value != settings.useFba) {
+                  context
+                      .read(searchSettingsControllerProvider)
+                      .update(useFba: value);
+                }
+              },
+            ),
+            SwitchListTile(
+              value: settings.priorFba,
+              title: const Text("FBA 出品を優先表示"),
+              onChanged: (value) {
+                if (value != settings.priorFba) {
+                  context
+                      .read(searchSettingsControllerProvider)
+                      .update(priorFba: value);
+                }
+              },
+            ),
+          ],
+        ).toList(),
+      ),
     );
   }
 }
 
-class _CodeType extends StatelessWidget {
+class _CodeType extends HookWidget {
   const _CodeType({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final settings = useProvider(_currentSettingsProvider);
     return ListTile(
       title: Row(
         children: [
           const Text("コードタイプ"),
           const Spacer(),
           DropdownButton<SearchType>(
-            value: SearchType.jan, // TODO: 仮
+            value: settings.type,
             items: const [
               DropdownMenuItem(
                 child: Text("JAN"),
@@ -81,7 +101,11 @@ class _CodeType extends StatelessWidget {
               ),
             ],
             onChanged: (value) {
-              // TODO: 変更に対応
+              if (value != settings.type) {
+                context
+                    .read(searchSettingsControllerProvider)
+                    .update(type: value);
+              }
             },
           )
         ],
@@ -90,18 +114,19 @@ class _CodeType extends StatelessWidget {
   }
 }
 
-class _UsedSubCondition extends StatelessWidget {
+class _UsedSubCondition extends HookWidget {
   const _UsedSubCondition({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final settings = useProvider(_currentSettingsProvider);
     return ListTile(
       title: Row(
         children: [
           const Text("中古コンディション"),
           const Spacer(),
           DropdownButton<UsedSubCondition>(
-            value: UsedSubCondition.all, // TODO: 仮
+            value: settings.usedSubCondition,
             items: const [
               DropdownMenuItem(
                 child: Text("すべて"),
@@ -125,7 +150,11 @@ class _UsedSubCondition extends StatelessWidget {
               ),
             ],
             onChanged: (value) {
-              // TODO: 変更対応
+              if (value != settings.usedSubCondition) {
+                context
+                    .read(searchSettingsControllerProvider)
+                    .update(usedSubCondition: value);
+              }
             },
           )
         ],
