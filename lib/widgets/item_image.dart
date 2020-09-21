e6c3cb4b57608aa3c0ee8e7244a5c4d0975cfcb4
limenote135 <17950108+limenote135@users.noreply.github.com>
2020-09-21 21:51:58 +0900

    画像表示用のウィジェット用意

---
+++ lib/widgets/item_image.dart
@@ -0,0 +1,41 @@
+import 'dart:typed_data';
+import 'dart:ui';
+
+import 'package:extended_image/extended_image.dart';
+import 'package:flutter/material.dart';
+
+class ItemImage extends StatelessWidget {
+  const ItemImage({Key key, this.url, this.data, this.onComplete})
+      : super(key: key);
+  final String url;
+  final Uint8List data;
+  final void Function(Future<ByteData> bytes) onComplete;
+
+  @override
+  Widget build(BuildContext context) {
+    if (data == null) {
+      print("image from internet");
+      return ExtendedImage.network(
+        url,
+        cache: true,
+        fit: BoxFit.scaleDown,
+        loadStateChanged: (state) {
+          if (state.extendedImageLoadState != LoadState.completed) {
+            return null;
+          }
+
+          if (state.extendedImageInfo != null) {
+            onComplete?.call(state.extendedImageInfo.image
+                .toByteData(format: ImageByteFormat.png));
+          }
+          return state.completedWidget;
+        },
+      );
+    }
+    print("image from binary");
+    return ExtendedImage.memory(
+      data,
+      fit: BoxFit.scaleDown,
+    );
+  }
+}
