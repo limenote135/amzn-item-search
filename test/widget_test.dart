d816d52270b8998d48d97fa9e24b1a839529497f
limenote135 <17950108+limenote135@users.noreply.github.com>
2020-09-21 17:51:45 +0900

    BottomNavigationBar で3つのページを切り替えるように

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,9 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:ama_search/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:ama_search/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
