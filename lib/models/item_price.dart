16c44fa013f7f8f5c42da9d5418e7a4d05038615
limenote135 <17950108+limenote135@users.noreply.github.com>
2020-09-21 21:09:39 +0900

    商品情報と価格情報を取得する Provider 追加

--- lib/models/item_price.dart
+++ lib/models/item_price.dart
@@ -1,8 +1,57 @@
-import 'package:ama_search/models/fee_info.dart';
+import 'package:ama_search/repository/mws.dart';
 import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hooks_riverpod/all.dart';
+
+import 'fee_info.dart';
+import 'search_condition.dart';
 
 part 'item_price.freezed.dart';
+
+final itemPricesFutureProvider =
+    FutureProvider.autoDispose.family<ItemPrices, String>((ref, asin) async {
+  final mws = ref.read(mwsRepositoryProvider);
+
+  final newPricesF =
+      mws.getLowestOfferListingsForASIN([asin], SearchCondition.newItem);
+  final usedPricesF =
+      mws.getLowestOfferListingsForASIN([asin], SearchCondition.usedItem);
+  final feeInfoF =
+      mws.getMyFeesEstimate(asin, 1000, useFba: true).then((value) async {
+    if (value.fbaFees == -1 || value.fbaFees == null) {
+      return mws.getMyFeesEstimate(asin, 1000, useFba: false);
+    }
+    return value;
+  });
+
+  final newPrice = await newPricesF;
+  final usedPrice = await usedPricesF;
+  final feeInfo = await feeInfoF;
+
+  final newPriceInfo = ItemPrice(
+    lowestPrice: newPrice.lowestPrice,
+    shipping: newPrice.shipping,
+    prices: newPrice.prices,
+  );
+
+  final usedPriceInfo = ItemPrice(
+    lowestPrice: usedPrice.lowestPrice,
+    shipping: usedPrice.shipping,
+    prices: usedPrice.prices,
+  );
+
+  final ret = ItemPrices(
+    newPrice: newPriceInfo,
+    usedPrice: usedPriceInfo,
+    feeInfo: FeeInfo(
+        referralFeeRate: feeInfo.referralFeeRate,
+        variableClosingFee: feeInfo.variableClosingFee,
+        fbaFee: feeInfo.fbaFees),
+  );
+
+  ref.maintainState = true;
+  return ret;
+});
 
 @freezed
 abstract class ItemPrices with _$ItemPrices {
