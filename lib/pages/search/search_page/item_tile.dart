83456de15c7a33f00b06cfa2771f299ed7f27f27
limenote135 <17950108+limenote135@users.noreply.github.com>
2020-09-21 22:58:25 +0900

    価格情報を表示

--- lib/pages/search/search_page/item_tile.dart
+++ lib/pages/search/search_page/item_tile.dart
@@ -1,6 +1,8 @@
+import 'package:ama_search/models/item.dart';
 import 'package:ama_search/pages/search/search_page/providers.dart';
 import 'package:ama_search/styles/font.dart';
 import 'package:ama_search/widgets/item_image.dart';
+import 'package:ama_search/widgets/price_info.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/all.dart';
@@ -153,6 +155,7 @@ class _TileBody extends HookWidget {
             Text("セット数: ${item.quantity} 個", style: smallSize),
           ],
         ),
+        const PriceInfo(),
         Text(
           "検索日: ${DateTime.parse(items.searchDate).toLocal()}",
           style: smallSize,
