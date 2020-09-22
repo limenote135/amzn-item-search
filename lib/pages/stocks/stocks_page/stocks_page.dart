3f9c99666788fc3c66cd519ade524f556c573467
limenote135 <17950108+limenote135@users.noreply.github.com>
2020-09-22 23:52:42 +0900

    スライドしてアイテムを削除できるように

--- lib/pages/stocks/stocks_page/stocks_page.dart
+++ lib/pages/stocks/stocks_page/stocks_page.dart
@@ -4,6 +4,7 @@ import 'package:amasearch/pages/stocks/stocks_page/item_tile.dart';
 import 'package:amasearch/widgets/theme_divider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:hooks_riverpod/all.dart';
 
 class StocksPage extends StatelessWidget {
@@ -37,12 +38,57 @@ class _Body extends HookWidget {
                 overrides: [
                   currentStockItemProvider.overrideWithValue(item),
                 ],
-                child: const ItemTile(),
+                child: const SlidableDeleteTile(child: ItemTile()),
               )
           ],
         ).toList(),
         const ThemeDivider(),
       ],
+    );
+  }
+}
+
+class SlidableDeleteTile extends HookWidget {
+  const SlidableDeleteTile({Key key, @required this.child}) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final item = useProvider(currentStockItemProvider);
+    return Slidable(
+      actionPane: const SlidableDrawerActionPane(),
+      actionExtentRatio: 0.25,
+      secondaryActions: [
+        IconSlideAction(
+          caption: "削除",
+          color: Colors.red,
+          icon: Icons.delete,
+          onTap: () async {
+            final ok = await showDialog<bool>(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: const Text("アイテムの削除"),
+                content: const Text("在庫リストからアイテムを削除します"),
+                actions: [
+                  FlatButton(
+                    child: const Text("Cancel"),
+                    onPressed: () => Navigator.of(context).pop(false),
+                  ),
+                  FlatButton(
+                    child: const Text("OK"),
+                    onPressed: () => Navigator.of(context).pop(true),
+                  )
+                ],
+              ),
+            );
+            if (ok) {
+              context.read(stockItemListControllerProvider).remove([item]);
+            }
+          },
+        ),
+      ],
+      child: child,
     );
   }
 }
