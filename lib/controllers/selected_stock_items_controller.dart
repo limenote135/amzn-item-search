354bc63da76bd682984914aecef884f35731748d
limenote135 <17950108+limenote135@users.noreply.github.com>
2020-09-24 20:47:48 +0900

    長押しで在庫アイテムをまとめて削除できるように

---
+++ lib/controllers/selected_stock_items_controller.dart
@@ -0,0 +1,26 @@
+import 'package:amasearch/models/stock_item.dart';
+import 'package:hooks_riverpod/all.dart';
+
+final selectedStockItemsControllerProvider =
+    StateNotifierProvider((_) => SelectedStockItemsController());
+
+class SelectedStockItemsController extends StateNotifier<List<StockItem>> {
+  SelectedStockItemsController() : super([]);
+
+  void toggleItem(StockItem item) {
+    if (state.contains(item)) {
+      // remove
+      state = [
+        for (final s in state)
+          if (s != item) s
+      ];
+    } else {
+      // add
+      state = [item, ...state];
+    }
+  }
+
+  void removeAll() {
+    state = [];
+  }
+}
