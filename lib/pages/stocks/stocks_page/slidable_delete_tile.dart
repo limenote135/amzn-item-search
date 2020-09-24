3a7ccdcb31bca85b575ad9f30f688c967dc1c3d9
limenote135 <17950108+limenote135@users.noreply.github.com>
2020-09-24 20:47:48 +0900

    長押しで在庫アイテムをまとめて削除できるように

---
+++ lib/pages/stocks/stocks_page/slidable_delete_tile.dart
@@ -0,0 +1,51 @@
+import 'package:amasearch/controllers/stock_item_controller.dart';
+import 'package:amasearch/models/stock_item.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class SlidableDeleteTile extends HookWidget {
+  const SlidableDeleteTile({Key key, @required this.child}) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final item = useProvider(currentStockItemProvider);
+    return Slidable(
+      actionPane: const SlidableDrawerActionPane(),
+      actionExtentRatio: 0.25,
+      secondaryActions: [
+        IconSlideAction(
+          caption: "削除",
+          color: Colors.red,
+          icon: Icons.delete,
+          onTap: () async {
+            final ok = await showDialog<bool>(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: const Text("アイテムの削除"),
+                content: const Text("在庫リストからアイテムを削除します"),
+                actions: [
+                  FlatButton(
+                    child: const Text("Cancel"),
+                    onPressed: () => Navigator.of(context).pop(false),
+                  ),
+                  FlatButton(
+                    child: const Text("OK"),
+                    onPressed: () => Navigator.of(context).pop(true),
+                  )
+                ],
+              ),
+            );
+            if (ok) {
+              context.read(stockItemListControllerProvider).remove([item]);
+            }
+          },
+        ),
+      ],
+      child: child,
+    );
+  }
+}
