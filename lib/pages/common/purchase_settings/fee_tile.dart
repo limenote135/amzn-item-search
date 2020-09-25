624861d65b35b97f333bf10869ec4916f4c503d4
limenote135 <17950108+limenote135@users.noreply.github.com>
2020-09-26 01:10:10 +0900

    リファクタリング

---
+++ lib/pages/common/purchase_settings/fee_tile.dart
@@ -0,0 +1,84 @@
+import 'package:amasearch/controllers/purchase_settings_controller.dart';
+import 'package:amasearch/models/fee_info.dart';
+import 'package:amasearch/models/item.dart';
+import 'package:amasearch/util/util.dart';
+import 'package:amasearch/widgets/text_line_tile.dart';
+import 'package:amasearch/widgets/theme_divider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class FeeTile extends HookWidget {
+  const FeeTile({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final item = useProvider(currentAsinDataProvider);
+    final base = useProvider(currentPurchaseSettingsControllerProvider);
+    final useFba = useProvider(base.state.select((value) => value.useFba));
+    final sellPrice =
+        useProvider(base.state.select((value) => value.sellPrice));
+    final purchasePrice =
+        useProvider(base.state.select((value) => value.purchasePrice));
+
+    final totalFee = _calcTotalFee(
+      sellPrice: sellPrice,
+      purchasePrice: purchasePrice,
+      feeInfo: item.prices.feeInfo,
+      useFba: useFba,
+    );
+
+    final feeRate = (item.prices.feeInfo.referralFeeRate * 100).round();
+
+    final sellFee =
+        _calcSellFee(sellPrice, item.prices.feeInfo.referralFeeRate);
+    final categoryFee = item.prices.feeInfo.variableClosingFee;
+    final fbaFee = useFba ? item.prices.feeInfo.fbaFee : 0;
+
+    return ExpansionTile(
+      onExpansionChanged: (value) {
+        unfocus();
+      },
+      title: TextLine(
+        leading: const Text("手数料"),
+        main: Text("${numberFormatter.format(totalFee)} 円"),
+      ),
+      children: [
+        const ThemeDivider(),
+        ListTile(
+          title: Column(
+            children: [
+              TextLine(
+                leading: Text("販売手数料($feeRate%)"),
+                main: Text("${numberFormatter.format(sellFee)} 円"),
+              ),
+              TextLine(
+                leading: const Text("カテゴリ成約料"),
+                main: Text("${numberFormatter.format(categoryFee)} 円"),
+              ),
+              TextLine(
+                  leading: const Text("FBA手数料"),
+                  main: Text("${numberFormatter.format(fbaFee)} 円")),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  int _calcSellFee(int sellPrice, double feeRate) {
+    return (sellPrice * feeRate).round();
+  }
+
+  int _calcTotalFee({
+    @required int sellPrice,
+    @required int purchasePrice,
+    @required FeeInfo feeInfo,
+    @required bool useFba,
+  }) {
+    final sellFee = _calcSellFee(sellPrice, feeInfo.referralFeeRate);
+    final fbaFee = useFba ? feeInfo.fbaFee : 0;
+
+    return sellFee + feeInfo.variableClosingFee + fbaFee;
+  }
+}
---
+++ lib/pages/common/purchase_settings/form.dart
@@ -0,0 +1,86 @@
+import 'package:amasearch/controllers/purchase_settings_controller.dart';
+import 'package:amasearch/pages/common/purchase_settings/image_tile.dart';
+import 'package:amasearch/util/util.dart';
+import 'package:amasearch/widgets/theme_divider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'fee_tile.dart';
+import 'input_prices_tile.dart';
+import 'input_purchase_amount_tile.dart';
+import 'item_condition_tile.dart';
+import 'profit_tile.dart';
+
+class PurchaseSettingsForm extends HookWidget {
+  const PurchaseSettingsForm({Key key, this.action}) : super(key: key);
+
+  final Widget action;
+
+  @override
+  Widget build(BuildContext context) {
+    final base = useProvider(currentPurchaseSettingsControllerProvider);
+    final formKey = useProvider(base.state.select((value) => value.formKey));
+    final useFba = useProvider(base.state.select((value) => value.useFba));
+    final sku = useProvider(base.state.select((value) => value.sku));
+    final memo = useProvider(base.state.select((value) => value.memo));
+
+    return Form(
+      key: formKey,
+      onChanged: () {
+        final form = Form.of(primaryFocus.context); //TODO: これでよいのか？
+        if (form.validate()) {
+          form.save();
+        }
+      },
+      child: ListView(
+        children: [
+          const ImageTile(),
+          const ThemeDivider(),
+          const InputPricesTile(),
+          SwitchListTile(
+            value: useFba,
+            title: const Text("FBA を利用する"),
+            onChanged: (value) {
+              unfocus();
+              context.read(base).update(useFba: value);
+            },
+          ),
+          const InputPurchaseAmoutTile(),
+          const ProfitTile(),
+          const FeeTile(),
+          const ThemeDivider(),
+          const ItemConditionTile(),
+          ListTile(
+            title: TextFormField(
+              initialValue: sku, // TODO: 初期値検討
+              keyboardType: TextInputType.url,
+              decoration: const InputDecoration(labelText: "SKU"),
+              textAlign: TextAlign.start,
+              onSaved: (newValue) {
+                if (newValue != sku) {
+                  context.read(base).update(sku: sku);
+                }
+              },
+            ),
+          ),
+          ListTile(
+            title: TextFormField(
+              maxLines: null,
+              initialValue: memo, // TODO: 初期値検討
+              keyboardType: TextInputType.multiline,
+              decoration: const InputDecoration(labelText: "メモ"),
+              textAlign: TextAlign.start,
+              onSaved: (newValue) {
+                if (newValue != memo) {
+                  context.read(base).update(memo: newValue);
+                }
+              },
+            ),
+          ),
+          if (action != null) action,
+        ],
+      ),
+    );
+  }
+}
