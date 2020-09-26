4483f0a58c5a84f4b8008382f1f1ae25b5d207b6
limenote135 <17950108+limenote135@users.noreply.github.com>
2020-09-26 12:33:21 +0900

    コミット漏れ

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
 
 import 'app.dart';
 
 void main() {
+  // TODO:
+  Intl.defaultLocale = 'ja_JP';
   runApp(ProviderScope(child: MyApp()));
 }
