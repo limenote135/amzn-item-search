b974a38337c0d7bd1bcef336d979a340d848612f
limenote135 <17950108+limenote135@users.noreply.github.com>
2020-09-27 16:23:49 +0900

    検索ページに検索設定を表示

--- lib/models/enums/search_type.dart
+++ lib/models/enums/search_type.dart
@@ -14,3 +14,19 @@ enum SearchType {
   @HiveField(3)
   tsutaya,
 }
+
+extension SearchTypeExtension on SearchType {
+  String toDisplayString() {
+    switch (this) {
+      case SearchType.jan:
+        return "JAN";
+      case SearchType.bookoff:
+        return "BOOK OFF";
+      case SearchType.geo:
+        return "GEO";
+      case SearchType.tsutaya:
+        return "TSUTAYA";
+    }
+    throw Exception("Unknown SearchType: $this");
+  }
+}
