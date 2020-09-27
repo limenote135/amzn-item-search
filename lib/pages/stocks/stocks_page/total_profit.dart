cd171372e52d8af02308c8665fc063eda45629c4
limenote135 <17950108+limenote135@users.noreply.github.com>
2020-09-27 16:28:41 +0900

    仕入れ商品のトータル利益を表示

---
+++ lib/pages/stocks/stocks_page/total_profit.dart
@@ -0,0 +1,31 @@
+import 'package:amasearch/controllers/stock_item_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class TotalProfit extends HookWidget {
+  const TotalProfit({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final items = useProvider(stockItemListControllerProvider.state);
+
+    var itemCount = 0;
+    var profitValue = 0;
+
+    for (final item in items) {
+      itemCount += item.amount;
+      profitValue += item.profitPerItem * item.amount;
+    }
+    return Row(
+      children: [
+        Expanded(
+          child: Text("商品数合計: $itemCount 個", textAlign: TextAlign.center),
+        ),
+        Expanded(
+          child: Text("粗利益合計: $profitValue 円", textAlign: TextAlign.center),
+        ),
+      ],
+    );
+  }
+}
