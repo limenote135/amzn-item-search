f958b84506c4e57f171332e427f3bc6e7962490d
limenote135 <17950108+limenote135@users.noreply.github.com>
2020-10-01 22:41:15 +0900

    アプリのバージョン、ライセンス表記を追加

--- lib/pages/settings/settings_page/settings_page.dart
+++ lib/pages/settings/settings_page/settings_page.dart
@@ -1,10 +1,42 @@
+import 'package:amasearch/widgets/theme_divider.dart';
 import 'package:flutter/material.dart';
+import 'package:package_info/package_info.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("設定"),
+      ),
+      body: const _Body(),
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        ListTile(
+          title: const Text("このアプリについて"),
+          onTap: () async {
+            final packageInfo = await PackageInfo.fromPlatform();
+            showAboutDialog(
+              context: context,
+              applicationName: packageInfo.appName,
+              applicationVersion: packageInfo.version,
+              applicationLegalese: "Copyright けんず",
+            );
+          },
+        ),
+        const ThemeDivider(),
+      ],
+    );
   }
 }
