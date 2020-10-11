5f42ecf7b9db17b5cbc11a96ef8bcc87c31a14b3
limenote135 <17950108+limenote135@users.noreply.github.com>
2020-10-11 15:07:59 +0900

    読み上げ設定画面作成

---
+++ lib/pages/settings/read_aloud_page/slider_tile.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class SliderTile extends StatefulWidget {
+  const SliderTile({
+    @required this.title,
+    @required this.initValue,
+    this.min = 0,
+    @required this.max,
+    @required this.divisions,
+    this.onSubmit,
+  });
+
+  final String title;
+  final double initValue;
+  final double min;
+  final double max;
+  final int divisions;
+  final void Function(double) onSubmit;
+
+  @override
+  _SliderTileState createState() => _SliderTileState(initValue);
+}
+
+class _SliderTileState extends State<SliderTile> {
+  _SliderTileState(this._value);
+
+  double _value;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("${widget.title}: $_value"),
+          Slider.adaptive(
+            value: _value,
+            min: widget.min,
+            max: widget.max,
+            divisions: widget.divisions,
+            onChanged: (value) {
+              setState(() {
+                _value = (value * 10).round() / 10;
+              });
+            },
+            onChangeEnd: (value) {
+              widget.onSubmit?.call((value * 10).round() / 10);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/util/text_to_speech.dart
@@ -0,0 +1,50 @@
+import 'package:amasearch/controllers/general_settings_controller.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final ttsProvider = Provider((ref) => TextToSpeech(ref.read));
+
+class TextToSpeech {
+  TextToSpeech(this._reader) : _flutterTts = FlutterTts() {
+    final settings = _reader(generalSettingsControllerProvider.state);
+    Future(() async {
+      final dynamic languages = await _flutterTts.getLanguages;
+      for (final lang in languages) {
+        if (lang == "ja-JP") {
+          await _flutterTts.setLanguage("ja-JP");
+          break;
+        }
+      }
+
+      await _flutterTts.setVolume(settings.readAloudVolume);
+      if (settings.readAloudSpeed != null) {
+        await _flutterTts.setSpeechRate(settings.readAloudSpeed);
+      } else {
+        final range = await getRange();
+        await _flutterTts.setSpeechRate(range.normal);
+
+        _reader(generalSettingsControllerProvider)
+            .update(readAloudSpeed: range.normal);
+      }
+    });
+  }
+
+  final FlutterTts _flutterTts;
+  final Reader _reader;
+
+  void speak(String text) {
+    _flutterTts.speak(text);
+  }
+
+  Future<void> setVolume(double volume) {
+    return _flutterTts.setVolume(volume);
+  }
+
+  Future<void> setSpeed(double speed) {
+    return _flutterTts.setSpeechRate(speed);
+  }
+
+  Future<SpeechRateValidRange> getRange() {
+    return _flutterTts.getSpeechRateValidRange;
+  }
+}
