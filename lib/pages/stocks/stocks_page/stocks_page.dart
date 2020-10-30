d22c79fee9be9ba51a6cf16895b98029101b1122
limenote135 <17950108+limenote135@users.noreply.github.com>
2020-10-30 23:42:24 +0900

    日ごとの購入数と粗利益を表示する

--- lib/pages/stocks/stocks_page/stocks_page.dart
+++ lib/pages/stocks/stocks_page/stocks_page.dart
@@ -4,8 +4,10 @@ import 'package:amasearch/models/stock_item.dart';
 import 'package:amasearch/pages/stocks/common/item_delete_handler.dart';
 import 'package:amasearch/pages/stocks/detail_page/detail_page.dart';
 import 'package:amasearch/pages/stocks/stocks_page/item_tile.dart';
+import 'package:amasearch/pages/stocks/stocks_page/summary_tile.dart';
 import 'package:amasearch/pages/stocks/stocks_page/total_profit.dart';
 import 'package:amasearch/util/csv.dart';
+import 'package:amasearch/util/formatter.dart';
 import 'package:amasearch/util/with_underline.dart';
 import 'package:amasearch/widgets/theme_divider.dart';
 import 'package:flutter/material.dart';
@@ -142,16 +144,73 @@ class _Body extends HookWidget {
           child: ListView.separated(
             separatorBuilder: (context, index) => const ThemeDivider(),
             itemCount: items.length,
-            itemBuilder: (context, index) => ProviderScope(
-              overrides: [
-                currentStockItemProvider.overrideWithValue(items[index]),
-              ],
-              child: index != items.length - 1 ? tile : WithUnderLine(tile),
-            ),
+            itemBuilder: (context, index) {
+              final tileImpl = ProviderScope(
+                overrides: [
+                  currentStockItemProvider.overrideWithValue(items[index]),
+                ],
+                child: index != items.length - 1 ? tile : WithUnderLine(tile),
+              );
+
+              final summary = _getSummary(items, index);
+              if (summary != null) {
+                return Column(
+                  children: [
+                    summary,
+                    const ThemeDivider(),
+                    tileImpl,
+                  ],
+                );
+              }
+
+              return tileImpl;
+            },
           ),
         ),
       ],
     );
+  }
+
+  Widget _getSummary(List<StockItem> items, int index) {
+    final current =
+        DateTime.parse(items[index].purchaseDate).toLocal().dayFormat();
+    if (index == 0) {
+      final lastIndex = _getLastIndex(items, 0, current);
+      return ProviderScope(
+        overrides: [
+          dateItemsProvider
+              .overrideWithValue(items.getRange(0, lastIndex).toList())
+        ],
+        child: const SummaryTile(),
+      );
+    }
+
+    final above =
+        DateTime.parse(items[index - 1].purchaseDate).toLocal().dayFormat();
+
+    if (above != current) {
+      final lastIndex = _getLastIndex(items, index, current);
+      return ProviderScope(
+        overrides: [
+          dateItemsProvider
+              .overrideWithValue(items.getRange(index, lastIndex).toList())
+        ],
+        child: const SummaryTile(),
+      );
+    }
+
+    return null;
+  }
+
+  int _getLastIndex(List<StockItem> items, int startIndex, String day) {
+    for (var i = startIndex + 1; i < items.length; i++) {
+      final nextDay =
+          DateTime.parse(items[i].purchaseDate).toLocal().dayFormat();
+      if (day != nextDay) {
+        return i;
+      }
+    }
+    return items.length;
   }
 }
 
