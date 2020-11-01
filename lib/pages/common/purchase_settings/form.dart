81e9817269c44fbdca9caf02eea168528b0a3b2f
limenote135 <17950108+limenote135@users.noreply.github.com>
2020-11-01 11:48:13 +0900

    仕入れ画面でも出品一覧を見れるように変更

--- lib/pages/common/purchase_settings/form.dart
+++ lib/pages/common/purchase_settings/form.dart
@@ -2,8 +2,10 @@ import 'dart:typed_data';
 
 import 'package:amasearch/controllers/purchase_settings_controller.dart';
 import 'package:amasearch/pages/common/purchase_settings/image_tile.dart';
+import 'package:amasearch/pages/search/common/seller_list_tile.dart';
 import 'package:amasearch/util/util.dart';
 import 'package:amasearch/widgets/theme_divider.dart';
+import 'package:amasearch/widgets/with_underline.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -39,9 +41,12 @@ class PurchaseSettingsForm extends HookWidget {
       },
       child: ListView(
         children: [
-          ImageTile(
-            onComplete: onComplete,
+          WithUnderLine(
+            ImageTile(
+              onComplete: onComplete,
+            ),
           ),
+          const SellerListTile(),
           const ThemeDivider(),
           const InputPricesTile(),
           SwitchListTile(
