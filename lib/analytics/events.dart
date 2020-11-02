42cf6696954f34b62b34bc6650c03663d1a5b3f1
limenote135 <17950108+limenote135@users.noreply.github.com>
2020-11-03 00:23:21 +0900

    Firebase Analytics イベントを設定

---
+++ lib/analytics/events.dart
@@ -0,0 +1,21 @@
+const purchaseEventName = "purchase";
+const pushSearchButtonEventName = "pushSearchButton";
+const searchEventName = "search";
+
+const shareEventName = "share";
+const deleteSearchHistoryEventName = "deleteSearchHistory";
+const deleteAllSearchHistoryEventName = "deleteAllSearchHistory";
+
+const searchEventJan = "searchJan";
+const searchEventWord = "searchWord";
+const searchEventBookoff = "searchBookoff";
+const searchEventGeo = "searchGeo";
+const searchEventTsutaya = "searchTsutaya";
+
+const pushSearchButtonAmazonName = "Amazon";
+const pushSearchButtonKeepaName = "Keepa";
+const pushSearchButtonDeltaName = "Delta";
+const pushSearchButtonKeezonName = "Keezon";
+const pushSearchButtonSellerCentralName = "SellerCentral";
+const pushSearchButtonAmazonNewListName = "AmazonNewList";
+const pushSearchButtonAmazonUsedListName = "AmazonusedList";
--- lib/repository/analytics.dart
+++
@@ -1,8 +0,0 @@
-import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_analytics/observer.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-final analyticsProvider = Provider((_) => FirebaseAnalytics());
-
-final analyticsObserverProvider = Provider(
-    (ref) => FirebaseAnalyticsObserver(analytics: ref.read(analyticsProvider)));
