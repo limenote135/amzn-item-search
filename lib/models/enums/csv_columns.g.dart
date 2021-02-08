06121319379baaf3b0ded22d73180cf1b25bdcf0
limenote135 <17950108+limenote135@users.noreply.github.com>
2021-02-08 21:31:18 +0900

    CSV 設定ページを追加

---
+++ lib/models/enums/csv_columns.g.dart
@@ -0,0 +1,106 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'csv_columns.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class CsvColumnAdapter extends TypeAdapter<CsvColumn> {
+  @override
+  final int typeId = 53;
+
+  @override
+  CsvColumn read(BinaryReader reader) {
+    switch (reader.readByte()) {
+      case 0:
+        return CsvColumn.asin;
+      case 1:
+        return CsvColumn.jan;
+      case 2:
+        return CsvColumn.title;
+      case 3:
+        return CsvColumn.sellPrice;
+      case 4:
+        return CsvColumn.purchasePrice;
+      case 5:
+        return CsvColumn.profit;
+      case 6:
+        return CsvColumn.listingPrice;
+      case 7:
+        return CsvColumn.quantity;
+      case 8:
+        return CsvColumn.condition;
+      case 9:
+        return CsvColumn.shipment;
+      case 10:
+        return CsvColumn.sku;
+      case 11:
+        return CsvColumn.retailer;
+      case 12:
+        return CsvColumn.comment;
+      case 13:
+        return CsvColumn.purchaseDate;
+      default:
+        return null;
+    }
+  }
+
+  @override
+  void write(BinaryWriter writer, CsvColumn obj) {
+    switch (obj) {
+      case CsvColumn.asin:
+        writer.writeByte(0);
+        break;
+      case CsvColumn.jan:
+        writer.writeByte(1);
+        break;
+      case CsvColumn.title:
+        writer.writeByte(2);
+        break;
+      case CsvColumn.sellPrice:
+        writer.writeByte(3);
+        break;
+      case CsvColumn.purchasePrice:
+        writer.writeByte(4);
+        break;
+      case CsvColumn.profit:
+        writer.writeByte(5);
+        break;
+      case CsvColumn.listingPrice:
+        writer.writeByte(6);
+        break;
+      case CsvColumn.quantity:
+        writer.writeByte(7);
+        break;
+      case CsvColumn.condition:
+        writer.writeByte(8);
+        break;
+      case CsvColumn.shipment:
+        writer.writeByte(9);
+        break;
+      case CsvColumn.sku:
+        writer.writeByte(10);
+        break;
+      case CsvColumn.retailer:
+        writer.writeByte(11);
+        break;
+      case CsvColumn.comment:
+        writer.writeByte(12);
+        break;
+      case CsvColumn.purchaseDate:
+        writer.writeByte(13);
+        break;
+    }
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CsvColumnAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
---
+++ lib/pages/settings/purchase_list_page/purchase_list_page.dart
@@ -0,0 +1,77 @@
+import 'package:amasearch/controllers/general_settings_controller.dart';
+import 'package:amasearch/models/enums/csv_columns.dart';
+import 'package:amasearch/widgets/theme_divider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/all.dart';
+
+class PurchaseListPage extends StatelessWidget {
+  const PurchaseListPage({Key key}) : super(key: key);
+  static const String routeName = "/settings/purchase_list";
+
+  static Route<void> route() {
+    return MaterialPageRoute(
+      settings: const RouteSettings(name: routeName),
+      builder: (context) => const PurchaseListPage(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("CSV設定"),
+      ),
+      body: const _Body(),
+    );
+  }
+}
+
+class _Body extends HookWidget {
+  const _Body({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final settings = useProvider(generalSettingsControllerProvider.state);
+    void _onReorder(int oldIndex, int newIndexRaw) {
+      var newIndex = newIndexRaw;
+      if (oldIndex < newIndex) {
+        // 移動前のインデックスより移動後のインデックスが大きい場合、アイテムの長さがリストの保有数よりも1大きくなってしまうため、
+        // newIndexから1引きます。
+        // 例えば、上の画像のように1番目のアイテムを3番目に移動した場合、oldIndex = 0, newIndex = 3となります。
+        // newIndexを1引いて2とします。
+        newIndex -= 1;
+      }
+      final item = settings.csvOrder[oldIndex];
+      final cp = settings.csvOrder
+        ..removeAt(oldIndex)
+        ..insert(newIndex, item);
+      context.read(generalSettingsControllerProvider).update(csvOrder: cp);
+    }
+
+    return Column(
+      children: [
+        ListTile(
+          title: Text(
+            "仕入れ商品リストをPCに送った際の並び順を変更できます。変更したい項目を長押ししてください。",
+            style: Theme.of(context).textTheme.caption,
+          ),
+        ),
+        const ThemeDivider(),
+        Expanded(
+          child: ReorderableListView(
+            onReorder: _onReorder,
+            children: [
+              for (final item in settings.csvOrder)
+                ListTile(
+                  title: Text(item.toDisplayString()),
+                  key: ValueKey(item),
+                  trailing: const Icon(Icons.menu),
+                ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
