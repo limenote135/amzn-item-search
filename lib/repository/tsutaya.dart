2416c4b39c31b479e6cc37cfd3659344b481c101
limenote135 <17950108+limenote135@users.noreply.github.com>
2021-02-19 17:02:14 +0900

    同じ JAN を読んだ際に毎回音声読み上げが行われるように Provider をリフレッシュする

--- lib/repository/tsutaya.dart
+++ lib/repository/tsutaya.dart
@@ -1,6 +1,5 @@
 import 'package:amasearch/controllers/item_controller.dart';
 import 'package:amasearch/models/item.dart';
-import 'package:amasearch/models/item_interceptor.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 const _tsutayaCodeLength = 16;
@@ -16,5 +15,5 @@ final tsutayaItemFutureProvider =
     FutureProvider.family<StateNotifierProvider<ItemController>, String>(
         (ref, code) async {
   final jan = getTsutayaJanCode(code);
-  return ref.watch(itemFutureProvider(InterceptorParams(code: jan)).future);
+  return ref.watch(itemFutureProvider(jan).future);
 });
