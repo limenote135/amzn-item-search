b31d15660d16f532c743aad43b6fbe89a73fe36b
limenote135 <17950108+limenote135@users.noreply.github.com>
2021-02-27 20:55:48 +0900

    StrongContainer を別ファイルに切り出し

---
+++ lib/widgets/strong_container.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class StrongContainer extends StatelessWidget {
+  const StrongContainer(this.child, {Key key}) : super(key: key);
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final newTextTheme = Theme.of(context).textTheme.apply(
+          bodyColor: Colors.black,
+          displayColor: Colors.black,
+        );
+    return Container(
+      color: Colors.red[100],
+      child: Theme(
+        data: Theme.of(context).copyWith(textTheme: newTextTheme),
+        child: child,
+      ),
+    );
+  }
+}
