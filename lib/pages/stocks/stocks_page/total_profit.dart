cd1e512d88952759032d7376695d0aed722d3990
limenote135 <17950108+limenote135@users.noreply.github.com>
2021-05-10 17:57:07 +0900

    桁区切りを追加

--- lib/pages/stocks/stocks_page/total_profit.dart
+++ lib/pages/stocks/stocks_page/total_profit.dart
@@ -1,4 +1,5 @@
 import 'package:amasearch/controllers/stock_item_controller.dart';
+import 'package:amasearch/util/formatter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -31,8 +32,10 @@ class TotalProfit extends HookWidget {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Text("総商品数: $itemCount 個", textAlign: TextAlign.center),
-            Text("仕入総額: $totalPurchase 円", textAlign: TextAlign.center),
-            Text("粗利総額: $profitValue 円", textAlign: TextAlign.center),
+            Text("仕入総額: ${numberFormatter.format(totalPurchase)} 円",
+                textAlign: TextAlign.center),
+            Text("粗利総額: ${numberFormatter.format(profitValue)}円",
+                textAlign: TextAlign.center),
           ],
         ),
         Row(
