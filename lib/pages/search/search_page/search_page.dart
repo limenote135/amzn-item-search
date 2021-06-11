df5a3eda3ae6c326fb0ac9d302b21eda452a0ac0
limenote135 <17950108+limenote135@users.noreply.github.com>
2021-06-11 15:31:23 +0900

    検索ページの AppBar を Sliver にする

--- lib/pages/search/search_page/search_page.dart
+++ lib/pages/search/search_page/search_page.dart
@@ -6,7 +6,6 @@ import 'package:amasearch/pages/search/search_page/search_bar.dart';
 import 'package:amasearch/util/util.dart';
 import 'package:amasearch/widgets/floating_action_margin.dart';
 import 'package:amasearch/widgets/theme_divider.dart';
-import 'package:amasearch/widgets/with_underline.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -22,8 +21,14 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const SearchBar(),
-      body: const _Body(),
+      body: const SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            _AppBar(),
+            _Body(),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         heroTag: onStartCameraHeroTag,
         onPressed: () {
@@ -39,36 +44,63 @@ class SearchPage extends StatelessWidget {
   }
 }
 
+GlobalKey _appBarKey = GlobalKey();
+
+class _AppBar extends HookWidget {
+  const _AppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final height = useState<double>(0);
+    WidgetsBinding.instance?.addPostFrameCallback((cb) {
+      // build が完了した後に AppBar の高さを計算して設定、再描画する
+      final obj = _appBarKey.currentContext?.findRenderObject();
+      if (obj != null) {
+        // ValueNotifier 内で値が同じなら set がスキップされるので、事前にチェックはしないでよい
+        height.value = obj.paintBounds.height;
+      }
+    });
+    return SliverAppBar(
+      title: Column(
+        key: _appBarKey,
+        children: const [
+          SearchBar(),
+          SearchSetting(),
+        ],
+      ),
+      centerTitle: true,
+      toolbarHeight: height.value,
+      floating: true,
+    );
+  }
+}
+
 class _Body extends HookWidget {
   const _Body({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final items = useProvider(searchItemControllerProvider);
-
-    return Column(
-      children: [
-        const WithUnderLine(SearchSetting()),
-        Expanded(
-          child: ListView.separated(
-            separatorBuilder: (context, index) => const ThemeDivider(),
-            itemCount: items.length + 1,
-            itemBuilder: (context, index) {
-              if (index == items.length) {
-                // FloatingActionButton 用のマージン
-                return floatingActionMargin;
-              }
-              return ProviderScope(
-                overrides: [
-                  currentFutureSearchItemProvider
-                      .overrideWithValue(items[index]),
-                ],
-                child: const ItemTile(),
-              );
-            },
-          ),
-        ),
-      ],
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          if (index == items.length * 2 + 1) {
+            // FloatingActionButton 用のマージン
+            return floatingActionMargin;
+          }
+          if (index.isEven || index == items.length * 2) {
+            return const ThemeDivider();
+          }
+          return ProviderScope(
+            overrides: [
+              currentFutureSearchItemProvider
+                  .overrideWithValue(items[index ~/ 2]),
+            ],
+            child: const ItemTile(),
+          );
+        },
+        childCount: items.length * 2 + 2,
+      ),
     );
   }
 }
