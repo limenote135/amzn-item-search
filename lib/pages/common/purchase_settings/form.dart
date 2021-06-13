56638e7b1b4190dace31fc3a16540e3f2dcf1769
limenote135 <17950108+limenote135@users.noreply.github.com>
2021-06-13 12:56:26 +0900

    仕入れ画面、編集画面で右上にボタンを追加

--- lib/pages/common/purchase_settings/form.dart
+++ lib/pages/common/purchase_settings/form.dart
@@ -8,7 +8,6 @@ import 'package:amasearch/widgets/theme_divider.dart';
 import 'package:amasearch/widgets/with_underline.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart' as base;
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 import 'package:reactive_touch_spin/reactive_touch_spin.dart';
@@ -51,7 +50,7 @@ final formValueProvider =
   });
 });
 
-class PurchaseSettingsForm extends HookWidget {
+class PurchaseSettingsForm extends StatelessWidget {
   const PurchaseSettingsForm({Key? key, this.action, this.onComplete})
       : super(key: key);
 
@@ -60,61 +59,56 @@ class PurchaseSettingsForm extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final item = useProvider(currentStockItemProvider);
-    final form = useProvider(formValueProvider(item));
-    return ReactiveForm(
-      formGroup: form.state,
-      child: _Unfocus(
-        child: ListView(
-          children: [
-            WithUnderLine(
-              ImageTile(
-                onComplete: onComplete,
-              ),
+    return _Unfocus(
+      child: ListView(
+        children: [
+          WithUnderLine(
+            ImageTile(
+              onComplete: onComplete,
             ),
-            const SellerListTile(),
-            const ThemeDivider(),
-            const InputPricesTile(),
-            const ItemConditionTile(),
-            ReactiveSwitchListTile(
-              formControlName: useFbaField,
-              title: const Text("FBA を利用"),
-            ),
-            ListTile(
-              title: Row(
-                children: [
-                  const Expanded(child: Text("個数")),
-                  Flexible(
-                    child: ReactiveTouchSpin(
-                      formControlName: quantityField,
-                      textStyle: const TextStyle(fontSize: 18),
-                      min: 1,
-                      max: 99,
-                      step: 1,
-                    ),
+          ),
+          const SellerListTile(),
+          const ThemeDivider(),
+          const InputPricesTile(),
+          const ItemConditionTile(),
+          ReactiveSwitchListTile(
+            formControlName: useFbaField,
+            title: const Text("FBA を利用"),
+          ),
+          ListTile(
+            title: Row(
+              children: [
+                const Expanded(child: Text("個数")),
+                Flexible(
+                  child: ReactiveTouchSpin(
+                    formControlName: quantityField,
+                    textStyle: const TextStyle(fontSize: 18),
+                    min: 1,
+                    max: 99,
+                    step: 1,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            const ProfitTile(),
-            const FeeTile(),
-            const TargetPriceTile(),
-            const RetailerTile(),
-            const PurchaseDateTile(),
-            const ThemeDivider(),
-            const SkuTile(),
-            ListTile(
-              title: ReactiveTextField<dynamic>(
-                formControlName: memoField,
-                maxLines: null,
-                keyboardType: TextInputType.multiline,
-                textAlign: TextAlign.start,
-                decoration: const InputDecoration(labelText: "メモ"),
-              ),
+          ),
+          const ProfitTile(),
+          const FeeTile(),
+          const TargetPriceTile(),
+          const RetailerTile(),
+          const PurchaseDateTile(),
+          const ThemeDivider(),
+          const SkuTile(),
+          ListTile(
+            title: ReactiveTextField<dynamic>(
+              formControlName: memoField,
+              maxLines: null,
+              keyboardType: TextInputType.multiline,
+              textAlign: TextAlign.start,
+              decoration: const InputDecoration(labelText: "メモ"),
             ),
-            if (action != null) action!,
-          ],
-        ),
+          ),
+          if (action != null) action!,
+        ],
       ),
     );
   }
