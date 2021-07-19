bc721bdffaf7d80b37989316c955d83742f0bc5b
limenote135 <17950108+limenote135@users.noreply.github.com>
2021-07-19 21:50:04 +0900

    自前のゲオコード変換を使うように変更

---
+++ lib/repository/geo.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'geo.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_GeoResponse _$_$_GeoResponseFromJson(Map<String, dynamic> json) {
+  return _$_GeoResponse(
+    code: json['code'] as String,
+    jan: json['jan'] as String? ?? '',
+  );
+}
+
+Map<String, dynamic> _$_$_GeoResponseToJson(_$_GeoResponse instance) =>
+    <String, dynamic>{
+      'code': instance.code,
+      'jan': instance.jan,
+    };
