08caa1c37fd7aa2cb32f6596f216b735e71de9b1
limenote135 <17950108+limenote135@users.noreply.github.com>
2021-07-23 13:25:25 +0900

    mac release 用のスクリプト追加

---
+++ mac_release.sh
@@ -0,0 +1,9 @@
+NAME=symbols_$1
+
+flutter clean
+rm -rf $NAME
+rm $NAME.zip
+flutter build ipa --release --build-number=$1 --obfuscate --split-debug-info=$NAME
+zip -r $NAME.zip $NAME
+
+open ./build/ios/archive/Runner.xcarchive
