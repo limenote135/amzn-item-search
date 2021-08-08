3a914ede01a9c2025b32fd80b39e10c2c37fa0f3
limenote135 <17950108+limenote135@users.noreply.github.com>
2021-08-08 13:15:07 +0900

    問い合わせにデバイス情報を含める

--- lib/pages/settings/support_page/suppert_page.dart
+++ lib/pages/settings/support_page/suppert_page.dart
@@ -1,10 +1,14 @@
+import 'dart:io';
+
 import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:amasearch/pages/login/common/validators.dart';
 import 'package:amasearch/util/auth.dart';
 import 'package:amasearch/util/cloud_functions.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:package_info/package_info.dart';
 
 class SupportPage extends StatelessWidget {
   const SupportPage({Key? key}) : super(key: key);
@@ -36,6 +40,16 @@ class _Body extends ConsumerWidget {
   static final emailKey = GlobalKey<FormFieldState<String>>();
   static final contentKey = GlobalKey<FormFieldState<String>>();
   static final formKey = GlobalKey<FormState>();
+
+  static Future<String> _getDeviceInfo() async {
+    if (Platform.isAndroid) {
+      final device = await DeviceInfoPlugin().androidInfo;
+      return "${Platform.operatingSystem}/${device.model} ${Platform.operatingSystemVersion}";
+    } else {
+      final device = await DeviceInfoPlugin().iosInfo;
+      return "${Platform.operatingSystem}/${device.utsname.machine} ${Platform.operatingSystemVersion}";
+    }
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -78,6 +92,12 @@ class _Body extends ConsumerWidget {
               if (formKey.currentState?.validate() ?? false) {
                 final email = emailKey.currentState!.value!;
                 final content = contentKey.currentState!.value!;
+
+                final info = await PackageInfo.fromPlatform();
+
+                final appVer = "Amasearch/${info.version}";
+                final device = await _getDeviceInfo();
+
                 try {
                   await EasyLoading.show(status: '送信中...');
                   final fn =
@@ -85,6 +105,7 @@ class _Body extends ConsumerWidget {
                   await fn.call<String>(<String, String>{
                     "mail": email,
                     "content": content,
+                    "device": "$appVer $device",
                   });
 
                   await EasyLoading.dismiss();
