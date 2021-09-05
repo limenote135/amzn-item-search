6d059b24a30876567a0787ffb2339485ad56a8b4
limenote135 <17950108+limenote135@users.noreply.github.com>
2021-09-05 12:33:13 +0900

    mac release スクリプト修正

--- mac_release.sh
+++ mac_release.sh
@@ -1,9 +1,11 @@
 NAME=symbols_$1
 
+rm ios/Podfile.lock
+rm -rf ios/Pods
 flutter clean
-rm -rf $NAME
+rm -rf "$NAME"
 rm $NAME.zip
 flutter build ipa --release --build-number=$1 --obfuscate --split-debug-info=$NAME
-zip -r $NAME.zip $NAME
+zip -r $NAME.zip "$NAME"
 
 open ./build/ios/archive/Runner.xcarchive
