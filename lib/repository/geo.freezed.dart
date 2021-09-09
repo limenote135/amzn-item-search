7d321d5b9cfc0fc4ede0382a052cacbfcd433e33
limenote135 <17950108+limenote135@users.noreply.github.com>
2021-09-10 00:38:07 +0900

    json_serializable の更新に対応

--- lib/repository/geo.freezed.dart
+++ lib/repository/geo.freezed.dart
@@ -1,5 +1,6 @@
+// coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
 
 part of 'geo.dart';
 
@@ -124,7 +125,7 @@ class _$_GeoResponse implements _GeoResponse {
   const _$_GeoResponse({required this.code, this.jan = ""});
 
   factory _$_GeoResponse.fromJson(Map<String, dynamic> json) =>
-      _$_$_GeoResponseFromJson(json);
+      _$$_GeoResponseFromJson(json);
 
   @override
   final String code;
@@ -160,7 +161,7 @@ class _$_GeoResponse implements _GeoResponse {
 
   @override
   Map<String, dynamic> toJson() {
-    return _$_$_GeoResponseToJson(this);
+    return _$$_GeoResponseToJson(this);
   }
 }
 
