7c76aeb280860320914342f0df3d409f9d887244
limenote135 <17950108+limenote135@users.noreply.github.com>
2021-10-18 22:40:17 +0900

    リファクタリング

--- lib/analytics/analytics.dart
+++ lib/analytics/analytics.dart
@@ -1,6 +1,4 @@
 import 'package:amasearch/models/enums/item_sub_condition.dart';
-import 'package:amasearch/models/enums/shortcut_type.dart';
-import 'package:amasearch/models/general_settings.dart';
 import 'package:amasearch/models/stock_item.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
@@ -69,45 +67,5 @@ class AnalyticsController {
 
   Future<void> setUserId(String? uid) {
     return _read(_analyticsProvider).setUserId(uid);
-  }
-
-  static String encodeShortcutToUserProp(List<ShortcutDetail> commands) {
-    // ユーザープロパティの値の最大は36文字
-    final buffer = StringBuffer("[");
-    for (var i = 0; i < commands.length; i++) {
-      if (commands[i].type != ShortcutType.none) {
-        final val = _createShortcutPropVal(commands[i]);
-        buffer
-          ..write("(")
-          ..write(val)
-          ..write(")");
-      }
-    }
-    buffer.write("]");
-    return buffer.toString();
-  }
-
-  static String _createShortcutPropVal(ShortcutDetail command) {
-    switch (command.type) {
-      case ShortcutType.none:
-        // ここにはこないはず
-        return "";
-      case ShortcutType.purchase:
-        return "buy";
-      case ShortcutType.delete:
-        return "del";
-      case ShortcutType.web:
-        return "web:${command.param}";
-      case ShortcutType.navigation:
-        switch (command.param) {
-          case navigationTargetNewOffers:
-            return "nav:new";
-          case navigationTargetUsedOffers:
-            return "nav:used";
-          case navigationTargetKeepa:
-            return "nav:kp";
-        }
-        return "nav:${command.param}";
-    }
   }
 }
