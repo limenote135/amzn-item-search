0c707432b0ebe28d7ecc2125043fd246376e5130
limenote135 <17950108+limenote135@users.noreply.github.com>
2021-10-18 22:40:17 +0900

    リファクタリング

---
+++ lib/pages/settings/shortcut_page/analytics.dart
@@ -0,0 +1,42 @@
+import 'package:amasearch/models/enums/shortcut_type.dart';
+import 'package:amasearch/models/general_settings.dart';
+
+String encodeShortcutToUserProp(List<ShortcutDetail> commands) {
+// ユーザープロパティの値の最大は36文字
+  final buffer = StringBuffer("[");
+  for (var i = 0; i < commands.length; i++) {
+    if (commands[i].type != ShortcutType.none) {
+      final val = _createShortcutPropVal(commands[i]);
+      buffer
+        ..write("(")
+        ..write(val)
+        ..write(")");
+    }
+  }
+  buffer.write("]");
+  return buffer.toString();
+}
+
+String _createShortcutPropVal(ShortcutDetail command) {
+  switch (command.type) {
+    case ShortcutType.none:
+      // ここにはこないはず
+      return "";
+    case ShortcutType.purchase:
+      return "buy";
+    case ShortcutType.delete:
+      return "del";
+    case ShortcutType.web:
+      return "web:${command.param}";
+    case ShortcutType.navigation:
+      switch (command.param) {
+        case navigationTargetNewOffers:
+          return "nav:new";
+        case navigationTargetUsedOffers:
+          return "nav:used";
+        case navigationTargetKeepa:
+          return "nav:kp";
+      }
+      return "nav:${command.param}";
+  }
+}
