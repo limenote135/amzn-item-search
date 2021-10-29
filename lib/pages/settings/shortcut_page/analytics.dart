2a8b5a8b746da16ca327c901d0b0c96a83aa7643
limenote135 <17950108+limenote135@users.noreply.github.com>
2021-10-29 14:13:46 +0900

    analytics の最大長処理を共通化

--- lib/pages/settings/shortcut_page/analytics.dart
+++ lib/pages/settings/shortcut_page/analytics.dart
@@ -2,7 +2,6 @@ import 'package:amasearch/models/enums/shortcut_type.dart';
 import 'package:amasearch/models/general_settings.dart';
 
 String encodeShortcutToUserProp(List<ShortcutDetail> commands) {
-// ユーザープロパティの値の最大は36文字
   final buffer = StringBuffer("[");
   for (var i = 0; i < commands.length; i++) {
     if (commands[i].type != ShortcutType.none) {
@@ -14,8 +13,7 @@ String encodeShortcutToUserProp(List<ShortcutDetail> commands) {
     }
   }
   buffer.write("]");
-  final ret = buffer.toString();
-  return ret.length > 36 ? ret.substring(0, 36) : ret;
+  return buffer.toString();
 }
 
 String _createShortcutPropVal(ShortcutDetail command) {
