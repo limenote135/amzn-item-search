141274ca3d8af6eb31f20c943e1fead8c8e9f033
limenote135 <17950108+limenote135@users.noreply.github.com>
2021-11-10 20:33:30 +0900

    windows でhotreload 対応

--- website/next.config.js
+++ website/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  webpack(config) {
+    return {
+      ...config,
+      watchOptions: {
+        aggregateTimeout: 300,
+        poll: 800,
+        ignored: ["node_modules", ".next"],
+      },
+    };
+  },
   reactStrictMode: true,
 };
