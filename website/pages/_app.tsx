7bf0d07949451324ca661e04157305ff70fbbbff
limenote135 <17950108+limenote135@users.noreply.github.com>
2022-02-04 20:54:45 +0900

    Google analytics　と OGP

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -7,6 +7,9 @@ import { CacheProvider, EmotionCache } from "@emotion/react";
 import theme from "@/styles/theme";
 import createEmotionCache from "@/styles/createEmotionCache";
 import Script from "next/script";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { pageview } from "@/plugin/gtags";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -16,6 +19,14 @@ interface MyAppProps extends AppProps {
 }
 
 export default function MyApp(props: MyAppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on("routeChangeComplete", pageview);
+    return () => {
+      router.events.off("routeChangeComplete", pageview);
+    };
+  }, [router.events]);
+
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
     <CacheProvider value={emotionCache}>
