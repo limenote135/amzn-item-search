895bfb2546f204396ef26f4c6e8ae1b5b6612f66
limenote135 <17950108+limenote135@users.noreply.github.com>
2022-02-05 23:26:45 +0900

    mac の release 用スクリプトを flavor 対応

--- mac_release.sh
+++ mac_release.sh
@@ -10,7 +10,7 @@ rm -rf ios/Pods
 flutter clean
 rm -rf "$NAME"
 rm "$NAME".zip
-flutter build ipa --release --build-number="$1"
+flutter build ipa --release --build-number="$1" --dart-define=FLAVOR=dev
 zip -r "$NAME".zip "$NAME"
 
 open ./build/ios/archive/Runner.xcarchive
