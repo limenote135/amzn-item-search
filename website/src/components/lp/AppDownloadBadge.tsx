c1a0db5f5423f9612990992c37da03ae02610dcc
limenote135 <17950108+limenote135@users.noreply.github.com>
2022-02-12 01:56:48 +0900

    ストアへのリンクを追加

--- website/src/components/lp/AppDownloadBadge.tsx
+++ website/src/components/lp/AppDownloadBadge.tsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
 import { Image } from "@/components/Image";
 import React from "react";
+import Link from "@/components/Link";
 
 type Props = {
   direction?: "column" | "row";
@@ -9,19 +10,27 @@ type Props = {
 
 const AppDownloadBadge = ({ direction = "row", scale = 1 }: Props) => (
   <Box alignItems={"center"} justifyContent={"center"} display={"flex"} flexDirection={direction}>
-    <Image
-      src={"/img/apple_store_badge.svg"}
-      width={179 * scale}
-      height={53 * scale}
-      alt={"iTunesStoreからダウンロード"}
-    />
+    <Link href={"https://apps.apple.com/jp/app/id1608782445"} target="_blank" rel="noopener noreferrer">
+      <Image
+        src={"/img/apple_store_badge.svg"}
+        width={179 * scale}
+        height={53 * scale}
+        alt={"iTunesStoreからダウンロード"}
+      />
+    </Link>
 
-    <Image
-      src={"/img/android_store_badge.png"}
-      width={200 * scale}
-      height={77 * scale}
-      alt={"GooglePlayからダウンロード"}
-    />
+    <Link
+      href={"https://play.google.com/store/apps/details?id=com.knzc.app.amasearch"}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Image
+        src={"/img/android_store_badge.png"}
+        width={200 * scale}
+        height={77 * scale}
+        alt={"GooglePlayからダウンロード"}
+      />
+    </Link>
   </Box>
 );
 
