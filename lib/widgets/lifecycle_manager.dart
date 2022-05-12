d1a9edb451cf2f2183e8a639f33616f80b774ba0
limenote135 <17950108+limenote135@users.noreply.github.com>
2022-05-12 16:47:45 +0900

    Flutter 3.0 更新に伴う linter 警告・エラー修正

--- lib/widgets/lifecycle_manager.dart
+++ lib/widgets/lifecycle_manager.dart
@@ -31,13 +31,13 @@ class _LifeCycleManagerState extends ConsumerState<LifecycleManager>
     with WidgetsBindingObserver {
   @override
   void initState() {
-    WidgetsBinding.instance?.addObserver(this);
+    WidgetsBinding.instance.addObserver(this);
     super.initState();
   }
 
   @override
   void dispose() {
-    WidgetsBinding.instance?.removeObserver(this);
+    WidgetsBinding.instance.removeObserver(this);
     super.dispose();
   }
 
