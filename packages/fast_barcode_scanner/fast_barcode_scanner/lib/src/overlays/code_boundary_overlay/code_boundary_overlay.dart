0f35da33661a7a45f5b5dba2ebc9d5ebe6c5d2f0
limenote135 <17950108+limenote135@users.noreply.github.com>
2022-05-18 22:38:39 +0900

    Revert "パッケージ更新"
    This reverts commit 96133b1c3c8aebd8bc47e9dc092c074be1afee31.

--- packages/fast_barcode_scanner/fast_barcode_scanner/lib/src/overlays/code_boundary_overlay/code_boundary_overlay.dart
+++ packages/fast_barcode_scanner/fast_barcode_scanner/lib/src/overlays/code_boundary_overlay/code_boundary_overlay.dart
@@ -30,7 +30,7 @@ class _CodeBoundaryOverlayState extends State<CodeBoundaryOverlay> {
           final analysisSize = _cameraController.analysisSize;
           if (analysisSize != null && barcodes.isNotEmpty) {
             return CustomPaint(
-              painter: CodeBorderPainter(
+              painter: CodeBoarderPainter(
                 imageSize: analysisSize,
                 barcodes: barcodes,
                 barcodePaintSelector: widget.codeBorderPaintBuilder,
