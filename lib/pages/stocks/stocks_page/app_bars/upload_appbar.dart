685462ba42b20ba2bbacb8e19c228fa65e59d111
limenote135 <17950108+limenote135@users.noreply.github.com>
2022-06-26 13:48:07 +0900

    CSV を転送する際に選択したものだけを含めるように

---
+++ lib/pages/stocks/stocks_page/app_bars/upload_appbar.dart
@@ -0,0 +1,45 @@
+import 'package:amasearch/controllers/general_settings_controller.dart';
+import 'package:amasearch/controllers/selected_stock_items_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'common.dart';
+import 'upload_csv.dart';
+
+class UploadAppBar extends ConsumerWidget implements PreferredSizeWidget {
+  const UploadAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final selectedItems = ref.watch(selectedStockItemsControllerProvider);
+    final settings = ref.read(generalSettingsControllerProvider);
+    return AppBar(
+      title: Text("${selectedItems.length} 件選択"),
+      leading: IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          resetState(ref);
+        },
+      ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: const StadiumBorder(),
+            ),
+            onPressed: selectedItems.isNotEmpty
+                ? () async {
+                    await uploadCsv(ref, selectedItems, settings.csvOrder);
+                  }
+                : null,
+            child: const Text("確定"),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size(double.infinity, kToolbarHeight);
+}
