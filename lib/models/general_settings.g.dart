afec3cccc90f26a88c95f148fe47757b80493849
limenote135 <17950108+limenote135@users.noreply.github.com>
2022-07-11 22:56:33 +0900

    Revert "Merged remove_delta into master"
    This reverts commit d0927d7995d08a1d1cd8a2b9a15669f6f7b1eaef, reversing
    changes made to 75d5e3b31ed546ece395372cf784617f1fcd875e.

--- lib/models/general_settings.g.dart
+++ lib/models/general_settings.g.dart
@@ -41,13 +41,18 @@ class GeneralSettingsAdapter extends TypeAdapter<_$_GeneralSettings> {
                   pattern:
                       'https://sellercentral.amazon.co.jp/abis/listing/syh?asin={asin}'),
               const CustomButtonDetail(
+                  id: 'bt03',
+                  enable: true,
+                  title: 'Delta',
+                  pattern: 'https://delta-tracer.com/item/detail/jp/{asin}/'),
+              const CustomButtonDetail(
                   id: 'bt04',
                   enable: true,
                   title: 'モノサーチ',
                   pattern: 'https://mnsearch.com/item?kwd={asin}'),
               const CustomButtonDetail(
                   id: 'bt05',
-                  enable: true,
+                  enable: false,
                   title: 'Keezon',
                   pattern: 'https://keezon.net/item/index?ASIN={asin}'),
               const CustomButtonDetail(
@@ -112,9 +117,7 @@ class GeneralSettingsAdapter extends TypeAdapter<_$_GeneralSettings> {
               const CustomButtonDetail(
                   id: 'bt19', enable: false, title: 'ボタン5', pattern: ''),
               const CustomButtonDetail(
-                  id: 'bt20', enable: false, title: 'ボタン6', pattern: ''),
-              const CustomButtonDetail(
-                  id: 'bt21', enable: false, title: 'ボタン7', pattern: '')
+                  id: 'bt20', enable: false, title: 'ボタン6', pattern: '')
             ]
           : (fields[10] as List).cast<CustomButtonDetail>(),
       csvOrder: fields[11] == null
