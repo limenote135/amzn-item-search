22be25658545eae92eb4083d07500bd773531dfd
limenote135 <17950108+limenote135@users.noreply.github.com>
2022-08-26 20:00:01 +0900

    ios build

--- mac_release.sh
+++ mac_release.sh
@@ -10,7 +10,16 @@ rm -rf ios/Pods
 flutter clean
 rm -rf "$NAME"
 rm "$NAME".zip
+arch -x86_64 pod repo update
+
+flutter pub get
+
+pushd ios
+arch -x86_64 pod install
+popd
+
 flutter build ipa --release --build-number="$1" --dart-define=FLAVOR=prod
 zip -r "$NAME".zip "$NAME"
 
-open ./build/ios/archive/Runner.xcarchive
+mv build/ios/ipa/アマサーチ.ipa build/ios/ipa/amasearch.ipa
+open build/ios/ipa
