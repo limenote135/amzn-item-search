9cac6b3c07ab757021a68b160dc42a312a9620bd
limenote135 <17950108+limenote135@users.noreply.github.com>
2022-09-03 17:20:29 +0900

    CSV の並びを設定する場所を別画面に

--- lib/pages/settings/csv_page/csv_page.dart
+++ lib/pages/settings/csv_page/csv_page.dart
@@ -1,12 +1,11 @@
-import 'package:amasearch/controllers/general_settings_controller.dart';
-import 'package:amasearch/models/enums/csv_columns.dart';
-import 'package:amasearch/widgets/theme_divider.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import 'order_page.dart';
+
 class PurchaseListPage extends StatelessWidget {
   const PurchaseListPage({super.key});
-  static const String routeName = "/settings/purchase_list";
+  static const String routeName = "/settings/csv";
 
   static Route<void> route() {
     return MaterialPageRoute(
@@ -31,44 +30,13 @@ class _Body extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final settings = ref.watch(generalSettingsControllerProvider);
-    void onReorder(int oldIndex, int newIndexRaw) {
-      var newIndex = newIndexRaw;
-      if (oldIndex < newIndex) {
-        // 移動前のインデックスより移動後のインデックスが大きい場合、アイテムの長さがリストの保有数よりも1大きくなってしまうため、
-        // newIndexから1引きます。
-        // 例えば、上の画像のように1番目のアイテムを3番目に移動した場合、oldIndex = 0, newIndex = 3となります。
-        // newIndexを1引いて2とします。
-        newIndex -= 1;
-      }
-      final item = settings.csvOrder[oldIndex];
-      final cp = settings.csvOrder.toList()
-        ..removeAt(oldIndex)
-        ..insert(newIndex, item);
-      ref.read(generalSettingsControllerProvider.notifier).update(csvOrder: cp);
-    }
-
-    return Column(
+    return ListView(
       children: [
         ListTile(
-          title: Text(
-            "仕入れ商品リストをPCに送った際の並び順を変更できます。変更したい項目を長押ししてください。",
-            style: Theme.of(context).textTheme.caption,
-          ),
-        ),
-        const ThemeDivider(),
-        Expanded(
-          child: ReorderableListView(
-            onReorder: onReorder,
-            children: [
-              for (final item in settings.csvOrder)
-                ListTile(
-                  title: Text(item.toDisplayString()),
-                  key: ValueKey(item),
-                  trailing: const Icon(Icons.menu),
-                ),
-            ],
-          ),
+          title: const Text("項目の並び順"),
+          onTap: () {
+            Navigator.push(context, OrderPage.route());
+          },
         ),
       ],
     );
---
+++ lib/pages/settings/csv_page/order_page.dart
@@ -0,0 +1,76 @@
+import 'package:amasearch/controllers/general_settings_controller.dart';
+import 'package:amasearch/models/enums/csv_columns.dart';
+import 'package:amasearch/widgets/theme_divider.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class OrderPage extends StatelessWidget {
+  const OrderPage({Key? key}) : super(key: key);
+  static const String routeName = "/settings/csv/order";
+
+  static Route<void> route() {
+    return MaterialPageRoute(
+      settings: const RouteSettings(name: routeName),
+      builder: (context) => const OrderPage(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("項目の並び順"),
+      ),
+      body: const _Body(),
+    );
+  }
+}
+
+class _Body extends ConsumerWidget {
+  const _Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final settings = ref.watch(generalSettingsControllerProvider);
+    void onReorder(int oldIndex, int newIndexRaw) {
+      var newIndex = newIndexRaw;
+      if (oldIndex < newIndex) {
+        // 移動前のインデックスより移動後のインデックスが大きい場合、アイテムの長さがリストの保有数よりも1大きくなってしまうため、
+        // newIndexから1引きます。
+        // 例えば、上の画像のように1番目のアイテムを3番目に移動した場合、oldIndex = 0, newIndex = 3となります。
+        // newIndexを1引いて2とします。
+        newIndex -= 1;
+      }
+      final item = settings.csvOrder[oldIndex];
+      final cp = settings.csvOrder.toList()
+        ..removeAt(oldIndex)
+        ..insert(newIndex, item);
+      ref.read(generalSettingsControllerProvider.notifier).update(csvOrder: cp);
+    }
+
+    return Column(
+      children: [
+        ListTile(
+          title: Text(
+            "仕入れ商品リストをPCに送った際の並び順を変更できます。変更したい項目を長押ししてください。",
+            style: Theme.of(context).textTheme.caption,
+          ),
+        ),
+        const ThemeDivider(),
+        Expanded(
+          child: ReorderableListView(
+            onReorder: onReorder,
+            children: [
+              for (final item in settings.csvOrder)
+                ListTile(
+                  title: Text(item.toDisplayString()),
+                  key: ValueKey(item),
+                  trailing: const Icon(Icons.menu),
+                ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
