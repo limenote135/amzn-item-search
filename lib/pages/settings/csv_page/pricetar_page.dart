08171bf82116e2f5b33a93ab2def5d4b5176256d
limenote135 <17950108+limenote135@users.noreply.github.com>
2022-09-17 11:16:38 +0900

    マカド設定ページを作成

--- lib/pages/settings/csv_page/pricetar_page.dart
+++ lib/pages/settings/csv_page/pricetar_page.dart
@@ -5,6 +5,8 @@ import 'package:amasearch/models/enums/pricetar_stopper.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import 'common.dart';
+
 class PricetarPage extends StatelessWidget {
   const PricetarPage({super.key});
   static const String routeName = "/settings/csv/pricetar";
@@ -201,46 +203,5 @@ class _Body extends ConsumerWidget {
         ),
       ],
     );
-  }
-
-  String createStopperText(RevisePriceStopper type, int value) {
-    switch (type) {
-      case RevisePriceStopper.nothing:
-        return "設定しない";
-      case RevisePriceStopper.listingPrice:
-        return "出品価格の$value%";
-      case RevisePriceStopper.profitValue:
-        return "$value円の利益";
-      case RevisePriceStopper.profitRate:
-        return "$value%の利益率";
-    }
-  }
-
-  String createStopperValueInputTitle(RevisePriceStopper type) {
-    switch (type) {
-      case RevisePriceStopper.nothing:
-        // ここにはこないはず
-        throw UnimplementedError("$type is not supported");
-      case RevisePriceStopper.listingPrice:
-        return "出品価格から設定";
-      case RevisePriceStopper.profitValue:
-        return "利益額から設定";
-      case RevisePriceStopper.profitRate:
-        return "利益率から設定";
-    }
-  }
-
-  String createStopperValueInputMessage(RevisePriceStopper type) {
-    switch (type) {
-      case RevisePriceStopper.nothing:
-        // ここにはこないはず
-        throw UnimplementedError("$type is not supported");
-      case RevisePriceStopper.listingPrice:
-        return "出品価格の何%をストッパーに設定しますか？";
-      case RevisePriceStopper.profitValue:
-        return "利益額が何円になるようにストッパーを設定しますか？";
-      case RevisePriceStopper.profitRate:
-        return "利益率が何%になるようにストッパーを設定しますか？";
-    }
   }
 }
