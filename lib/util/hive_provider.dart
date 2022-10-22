39bbb7a136badd57989e5c130b1d4d92bb84233d
limenote135 <17950108+limenote135@users.noreply.github.com>
2022-10-23 02:11:28 +0900

    ワード検索の履歴を表示

--- lib/util/hive_provider.dart
+++ lib/util/hive_provider.dart
@@ -1,6 +1,7 @@
 import 'package:amasearch/models/constants.dart';
 import 'package:amasearch/models/search_item.dart';
 import 'package:amasearch/models/stock_item.dart';
+import 'package:amasearch/models/word_search_history.dart';
 import 'package:hive/hive.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -11,3 +12,6 @@ final stockItemBoxProvider =
     Provider((_) => Hive.box<StockItem>(stockItemBoxName));
 
 final settingsBoxProvider = Provider((_) => Hive.box<dynamic>(settingsBoxName));
+
+final wordSearchHistoryBoxProvider =
+    Provider((_) => Hive.box<WordSearchHistory>(wordSearchHistoryBoxName));
