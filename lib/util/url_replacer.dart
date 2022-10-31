4ff7bd0907ebdeed955b07ad715ca333818bf888
limenote135 <17950108+limenote135@users.noreply.github.com>
2022-10-31 19:05:56 +0900

    カスタムボタンにオークファンの URL を入れた場合の特別対応

--- lib/util/url_replacer.dart
+++ lib/util/url_replacer.dart
@@ -1,12 +1,22 @@
 import 'package:amasearch/models/asin_data.dart';
+import 'package:convert/convert.dart';
+import 'package:euc/euc.dart';
 
 import 'variable_format.dart';
 
+final _encoder = EucJPEncoder();
+
 String replaceUrl({required String template, required AsinData item}) {
+  var title = item.title;
+  if (template.contains("aucfan.com")) {
+    // オークファンはタイトルを EUC エンコードするので特別対応
+    final eucText = _encoder.convert(item.title);
+    title = hex.encode(eucText);
+  }
   final url = template
       .replaceAll(asinVariable, item.asin)
       .replaceAll(janVariable, item.jan)
-      .replaceAll(titleVariable, item.title)
+      .replaceAll(titleVariable, title)
       .replaceAll(modelVariable, item.model);
 
   return Uri.encodeFull(url);
