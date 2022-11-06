041a80a5ea4349d64c2a1cbc1784962e3c632bf2
limenote135 <17950108+limenote135@users.noreply.github.com>
2022-11-06 13:47:07 +0900

    電卓のリファクタリング

--- lib/pages/common/purchase_settings/input_prices_tile.dart
+++ lib/pages/common/purchase_settings/input_prices_tile.dart
@@ -1,6 +1,6 @@
 import 'package:amasearch/pages/common/purchase_settings/values.dart';
-import 'package:amasearch/util/calculator.dart';
 import 'package:amasearch/util/custom_validator.dart';
+import 'package:amasearch/widgets/calculator.dart';
 import 'package:amasearch/widgets/theme_divider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
