78d4cc96e45d51332d3fffcba577ab300ad0d26e
limenote135 <limenote135@gmail.com>
2022-11-07 19:44:25 +0900

    在庫取得のオンオフ設定場所を移動

--- lib/pages/common/offer_listing_page/offer_listing_page.dart
+++ lib/pages/common/offer_listing_page/offer_listing_page.dart
@@ -1,3 +1,6 @@
+import 'package:amasearch/analytics/analytics.dart';
+import 'package:amasearch/analytics/properties.dart';
+import 'package:amasearch/controllers/general_settings_controller.dart';
 import 'package:amasearch/models/offer_listings.dart';
 import 'package:amasearch/widgets/async_value_widget.dart';
 import 'package:amasearch/widgets/theme_divider.dart';
@@ -13,6 +16,7 @@ final currentOfferListingParamProvider =
 
 class OfferListingPage extends StatelessWidget {
   const OfferListingPage({super.key});
+
   static const String routeName = "/offer_listing";
 
   static Route<void> route(OfferListingsParams params) {
@@ -45,37 +49,59 @@ class _Body extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final doGetStocks = ref.watch(
+      generalSettingsControllerProvider.select((value) => value.getStocks),
+    );
     final param = ref.watch(currentOfferListingParamProvider);
     final offerTotalCountAsyncValue = ref.watch(offerTotalCountProvider(param));
-    return AsyncValueWidget<int>(
-      value: offerTotalCountAsyncValue,
-      errorInfo: [
-        "OfferListingsPage._Body.offerTotalCountProvider",
-        "Param: ${param.toString()}",
-      ],
-      data: (value) {
-        if (value == 0) {
-          return ProviderScope(
-            overrides: [
-              currentAsinProvider.overrideWithValue(param.asin),
-            ],
-            child: const CartTile(),
-          );
-        }
-        return ListView.separated(
-          itemBuilder: (context, index) {
-            return ProviderScope(
-              overrides: [
-                currentAsinProvider.overrideWithValue(param.asin),
-                currentIndex.overrideWithValue(index),
-              ],
-              child: _selectWidgets(index, value),
-            );
+    return Column(
+      children: [
+        SwitchListTile(
+          title: const Text("在庫数を取得(β)"),
+          subtitle: const Text("表示が重くなります"),
+          value: doGetStocks,
+          onChanged: (value) {
+            ref
+                .read(generalSettingsControllerProvider.notifier)
+                .update(getStocks: value);
+            ref
+                .read(analyticsControllerProvider)
+                .setUserProp(getStocksPropName, value.toString());
           },
-          itemCount: value,
-          separatorBuilder: (context, index) => const ThemeDivider(),
-        );
-      },
+        ),
+        Expanded(
+          child: AsyncValueWidget<int>(
+            value: offerTotalCountAsyncValue,
+            errorInfo: [
+              "OfferListingsPage._Body.offerTotalCountProvider",
+              "Param: ${param.toString()}",
+            ],
+            data: (value) {
+              if (value == 0) {
+                return ProviderScope(
+                  overrides: [
+                    currentAsinProvider.overrideWithValue(param.asin),
+                  ],
+                  child: const CartTile(),
+                );
+              }
+              return ListView.separated(
+                itemBuilder: (context, index) {
+                  return ProviderScope(
+                    overrides: [
+                      currentAsinProvider.overrideWithValue(param.asin),
+                      currentIndex.overrideWithValue(index),
+                    ],
+                    child: _selectWidgets(index, value),
+                  );
+                },
+                itemCount: value,
+                separatorBuilder: (context, index) => const ThemeDivider(),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 
