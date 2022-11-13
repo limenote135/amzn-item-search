22230cee956aeddf5525723b512f9ebb1f075a7d
limenote135 <17950108+limenote135@users.noreply.github.com>
2022-11-13 14:23:07 +0900

    SignInWithApple を FlutterFire のみでお行う

--- lib/pages/login/common/sign_in_with_google.dart
+++ lib/pages/login/common/sign_in_with_google.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-Future<OAuthCredential?> signInWithGoogle() async {
+Future<UserCredential?> signInWithGoogle(FirebaseAuth auth) async {
   final googleUser = await GoogleSignIn().signIn();
   if (googleUser == null) {
     return null;
@@ -9,8 +9,10 @@ Future<OAuthCredential?> signInWithGoogle() async {
 
   final googleAuth = await googleUser.authentication;
 
-  return GoogleAuthProvider.credential(
+  final credential = GoogleAuthProvider.credential(
     accessToken: googleAuth.accessToken,
     idToken: googleAuth.idToken,
   );
+  final result = await auth.signInWithCredential(credential);
+  return result;
 }
