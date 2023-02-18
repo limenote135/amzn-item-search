8d2b58f20465193a439efcf9431cbaa555094b03
limenote135 <17950108+limenote135@users.noreply.github.com>
2023-02-18 23:47:39 +0900

    新品は画像を無視する

--- lib/util/listings.dart
+++ lib/util/listings.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:amasearch/models/enums/item_condition.dart';
 import 'package:amasearch/models/listing_item.dart';
 import 'package:archive/archive_io.dart';
 import 'package:dartx/dartx_io.dart';
@@ -11,11 +12,16 @@ const listingsFileVersion = "v1";
 
 Future<File> createListingsFile(List<ListingItem> items) async {
   // JSON に入れる出品画像にはファイル名のみを入れる
-  final newItems = items
-      .map(
-        (e) => e.copyWith(images: e.images.map((e) => File(e).name).toList()),
-      )
-      .toList();
+  final newItems = items.map(
+    (e) {
+      if (e.condition == ItemCondition.newItem) {
+        // 新品の場合は画像は無視する
+        return e.copyWith(images: []);
+      }
+      // 中古の場合はファイル名を入れる
+      return e.copyWith(images: e.images.map((e) => File(e).name).toList());
+    },
+  ).toList();
   final jsonText = jsonEncode(newItems);
 
   final tempDir = await getTemporaryDirectory();
@@ -33,6 +39,10 @@ Future<File> createListingsFile(List<ListingItem> items) async {
 
   final promises = <Future<File>>[];
   for (final item in items) {
+    if(item.condition == ItemCondition.newItem) {
+      // 新品の場合はコピー不要
+      continue;
+    }
     for (final img in item.images) {
       final f = File(img);
       final dest = "$fileDirPath/${f.name}";
