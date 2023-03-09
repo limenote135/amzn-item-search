23a09252844bc968af9c1f6b5e943a192b45da69
limenote135 <17950108+limenote135@users.noreply.github.com>
2023-03-09 22:12:29 +0900

    お知らせを表示できるようにする

--- lib/widgets/updater_widget.dart
+++ lib/widgets/updater_widget.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:adaptive_dialog/adaptive_dialog.dart';
+import 'package:amasearch/util/information.dart';
 import 'package:amasearch/util/release_notes.dart';
 import 'package:amasearch/util/version_checker.dart';
 import 'package:flutter/material.dart';
@@ -13,6 +14,8 @@ import 'lifecycle_manager.dart';
 
 final updateProvider =
     FutureProvider<bool>((_) => VersionChecker().needUpdate());
+
+final _onStartupProvider = StateProvider((ref) => true);
 
 class UpdateCheckObserver with LifecycleCallback {
   const UpdateCheckObserver();
@@ -37,22 +40,37 @@ class Updater extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final onStartup = ref.watch(_onStartupProvider);
     ref.listen<AsyncValue<bool>>(updateProvider, (_, value) {
-      value.whenData((needUpdate) {
-        _showUpdateDialog(needUpdate, context);
-        if (!needUpdate) {
-          _showReleaseNoteDialog(context);
+      value.whenData((needUpdate) async {
+        if (needUpdate) {
+          await _showUpdateDialog(context);
+        } else if (onStartup) {
+          // 起動直後の場合に呼ばれる
+          // お知らせは起動直後にのみ表示するため、フラグを落とすため、
+          // 以降は最小化から再表示した場合などは無視される
+          ref.read(_onStartupProvider.notifier).state = false;
+          final info = await Information().get();
+          if (info.isNotEmpty) {
+            await showOkAlertDialog(
+              context: context,
+              title: "お知らせ",
+              message: info,
+            );
+          } else {
+            // お知らせが無い場合は変更履歴のチェックを行う
+            await _showReleaseNoteDialog(context);
+          }
+        } else {
+          await _showReleaseNoteDialog(context);
         }
       });
     });
+
     return Container();
   }
 
-  Future<void> _showUpdateDialog(bool needUpdate, BuildContext context) async {
-    if (!needUpdate) {
-      return;
-    }
-
+  Future<void> _showUpdateDialog(BuildContext context) async {
     const title = "バージョン更新のお知らせ";
     const message = "新しいバージョンが利用可能です。"
         "更新した上で再度アプリを起動してください。";
