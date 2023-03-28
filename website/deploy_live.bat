8c0948396547635c7d4a2b2e9f03956746f5811c
limenote135 <17950108+limenote135@users.noreply.github.com>
2023-03-28 21:10:49 +0900

    [web] 環境ごとにファイルをコピーする

--- website/deploy_live.bat
+++ website/deploy_live.bat
@@ -1,3 +1,8 @@
+COPY /Y .\env_files\robots.live.txt .\public\robots.txt
+COPY /Y .env.live.production .env.production
+COPY /Y .\src\plugin\firebaseAuth.live.json .\src\plugin\firebaseAuth.json
+COPY /Y sentry.stg.properties sentry.properties
+
 docker compose -f .\docker-compose.release.yml -f .\docker-compose.release.live.yml up --build --force-recreate --remove-orphans
 docker ps -a
 pause
