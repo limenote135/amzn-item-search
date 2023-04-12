5c31f983efbd49cad60bbb5fd0dd1f4a1678dbe8
limenote135 <17950108+limenote135@users.noreply.github.com>
2023-04-12 23:42:27 +0900

    [web] トレースのサンプリング

--- website/sentry.client.config.js
+++ website/sentry.client.config.js
@@ -11,6 +11,15 @@ Sentry.init({
   dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
+  tracesSampler: samplingContext => {
+    if (samplingContext.parentSampled) {
+      return samplingContext.parentSampled;
+    }
+    if (samplingContext.location && samplingContext.location.pathname === "/") {
+      return 0.5;
+    }
+    return true;
+  },
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
