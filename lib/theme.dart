175bb39b377966e1fbaed6ac50f1e54d2dad7acb
limenote135 <17950108+limenote135@users.noreply.github.com>
2023-09-04 16:18:48 +0900

    extended_image　の更新 flex_color_schema の更新と背景色が変わることへ対応

--- lib/theme.dart
+++ lib/theme.dart
@@ -3,11 +3,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 const _schemaColor = FlexScheme.gold;
+const backgroundColor = Color(0xFFD8AD7E);
 
 final lightTheme = FlexColorScheme.light(
   scheme: _schemaColor,
   appBarStyle: FlexAppBarStyle.surface,
   visualDensity: FlexColorScheme.comfortablePlatformDensity,
+  //ignore: avoid_redundant_argument_values
+  useMaterial3: false,
 ).toTheme.copyWith(
       dividerTheme: const DividerThemeData(
         color: Colors.grey,
@@ -17,6 +20,8 @@ final lightTheme = FlexColorScheme.light(
 final darkTheme = FlexColorScheme.dark(
   scheme: _schemaColor,
   visualDensity: VisualDensity.adaptivePlatformDensity,
+  //ignore: avoid_redundant_argument_values
+  useMaterial3: false,
 ).toTheme.copyWith(
       dividerTheme: const DividerThemeData(
         color: Colors.white,
