b5461f1a9ba4ed299d6baa8964253e9ff7bf5dfb
limenote135 <17950108+limenote135@users.noreply.github.com>
2023-09-15 00:16:58 +0900

    過去の仕入れ履歴を表示できるように対応

--- lib/pages/common/seaech_item_detail/item_detail.dart
+++ lib/pages/common/seaech_item_detail/item_detail.dart
@@ -1,5 +1,8 @@
+import 'package:amasearch/controllers/stock_item_controller.dart';
 import 'package:amasearch/models/asin_data.dart';
 import 'package:amasearch/models/enums/item_condition.dart';
+import 'package:amasearch/models/stock_item.dart';
+import 'package:amasearch/pages/common/listing_history_page/listing_history_page.dart';
 import 'package:amasearch/pages/search/common/seller_list_tile.dart';
 import 'package:amasearch/styles/font.dart';
 import 'package:amasearch/util/formatter.dart';
@@ -8,6 +11,7 @@ import 'package:amasearch/widgets/item_image.dart';
 import 'package:amasearch/widgets/search_buttons.dart';
 import 'package:amasearch/widgets/strong_container.dart';
 import 'package:amasearch/widgets/text_line_tile.dart';
+import 'package:dartx/dartx.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -25,11 +29,17 @@ class SearchItemDetail extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final item = ref.watch(currentAsinDataProvider);
     final isRestricted = item.restrictions.newItem | item.restrictions.used;
+
+    final isStocked = ref.watch(
+      stockItemForAsinProvider(item.asin).select((value) => value.isNotEmpty),
+    );
+
     return ListView(
       children: ListTile.divideTiles(
         context: context,
         tiles: [
           if (isRestricted) const _Restricted(),
+          if (isStocked) const _StockInfo(),
           InkWell(
             onLongPress: () {
               Clipboard.setData(ClipboardData(text: item.title)).then((_) {
@@ -146,5 +156,53 @@ class _Restricted extends ConsumerWidget {
       return "(中古)";
     }
     return "(新/古)";
+  }
+}
+
+class _StockInfo extends ConsumerWidget {
+  const _StockInfo();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final item = ref.watch(currentAsinDataProvider);
+    final stockItems = ref.watch(stockItemForAsinProvider(item.asin));
+    return Strong2Container(
+      ListTile(
+        title: Column(
+          children: [
+            Text(
+              createText(
+                stockItems.length,
+                stockItems.where((e) => e.purchasePrice != 0).toList(),
+              ),
+            ),
+            Text.rich(
+              TextSpan(
+                text: "詳細はこちら",
+                style: const TextStyle(
+                  color: Colors.blue,
+                  decoration: TextDecoration.underline,
+                ),
+                recognizer: TapGestureRecognizer()
+                  ..onTap = () {
+                    Navigator.push(
+                      context,
+                      ListingHistoryPage.route(stockItems),
+                    );
+                  },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  String createText(int total, List<StockItem> items) {
+    if (items.isEmpty) {
+      return "過去に $total 回仕入れています";
+    }
+    final average = items.averageBy((e) => e.purchasePrice).round();
+    return "過去に平均 $average 円で $total 回仕入れています";
   }
 }
