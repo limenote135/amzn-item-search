4c20f5e152b10cd1c966396e05f9aa675425ac69
limenote135 <17950108+limenote135@users.noreply.github.com>
2023-10-28 23:57:13 +0900

    どのアラート条件に一致したのかがわかるように表示

--- lib/models/alert_condition.dart
+++ lib/models/alert_condition.dart
@@ -1,4 +1,5 @@
 import 'package:amasearch/models/constants.dart';
+import 'package:amasearch/repository/mws_category.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hive/hive.dart';
 
@@ -17,4 +18,36 @@ class AlertCondition with _$AlertCondition {
 
   factory AlertCondition.fromJson(Map<String, dynamic> json) =>
       _$AlertConditionFromJson(json);
+}
+
+extension AlertConditionExtension on AlertCondition {
+  String toSummary() {
+    switch (type) {
+      case AlertType.profit:
+        return "粗利 $value 円以上";
+      case AlertType.rank:
+        return "ランキング $value 位以内";
+      case AlertType.category:
+        if (mwsCategoryIdMap.containsValue(value)) {
+          final name = mwsCategoryIdMap.entries
+              .singleWhere((element) => element.value == value)
+              .key;
+          return "カテゴリ $name";
+        }
+        return "カテゴリ 不明";
+      case AlertType.condition:
+        final val = AlertOfferCondition.values[value];
+        return "状態 ${val.toDisplayString()}";
+      case AlertType.premium:
+        return "プレ値";
+      case AlertType.noAmazon:
+        return "Amazon販売無し";
+      case AlertType.noNewOffer:
+        return "新品無し";
+      case AlertType.newOfferCount:
+        return "新品出品 $value人以下";
+      case AlertType.usedOfferCount:
+        return "中古出品 $value人以下";
+    }
+  }
 }
