06f2b6f293bb03e74868a459463a701796d8f991
limenote135 <17950108+limenote135@users.noreply.github.com>
2023-11-05 10:02:26 +0900

    プランが Suspended の場合でもすべての機能が使えてしまう不具合を修正

--- lib/util/auth.dart
+++ lib/util/auth.dart
@@ -62,7 +62,9 @@ final currentClaimsProvider = StreamProvider((ref) async* {
         final token = await user.getIdTokenResult();
         final dynamic plan = token.claims?["pl"];
 
-        if (plan != null && plan != PlanType.free.name) {
+        if (plan != null &&
+            plan != PlanType.free.name &&
+            plan != PlanType.suspended.name) {
           // トライアル、標準、キャンペーンの場合
           ref.read(isPaidUserProvider.notifier).state = true;
           unawaited(
