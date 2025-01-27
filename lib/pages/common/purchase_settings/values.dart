6304108b704034779a1b65fab673e2a77522a498
limenote135 <limenote135@gmail.com>
2025-01-27 18:44:32 +0900

    deprecated コード修正

--- lib/pages/common/purchase_settings/values.dart
+++ lib/pages/common/purchase_settings/values.dart
@@ -1,6 +1,5 @@
 import 'package:amasearch/models/enums/purchase_item_condition.dart';
 import 'package:amasearch/util/util.dart';
-import 'package:collection/collection.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 
 const purchasePriceField = "purchasePrice";
@@ -23,17 +22,27 @@ String getString(AbstractControl<dynamic> form, String field) {
   switch (field) {
     case purchaseDateField:
       {
-        final date = f.control(purchaseDateField).value as DateTime?;
+        final date = f
+            .control(purchaseDateField)
+            .value as DateTime?;
         return currentTimeString(time: date);
       }
     case retailerField:
-      return f.control(retailerField).value as String;
+      return f
+          .control(retailerField)
+          .value as String;
     case skuField:
-      return f.control(skuField).value as String;
+      return f
+          .control(skuField)
+          .value as String;
     case memoField:
-      return f.control(memoField).value as String;
+      return f
+          .control(memoField)
+          .value as String;
     case conditionTextField:
-      return f.control(conditionTextField).value as String;
+      return f
+          .control(conditionTextField)
+          .value as String;
   }
   throw Exception("Invalid field: $field");
 }
@@ -43,16 +52,24 @@ int getInt(AbstractControl<dynamic> form, String field) {
   switch (field) {
     case purchasePriceField:
       {
-        final purchase = f.control(purchasePriceField).value as String? ?? "";
+        final purchase = f
+            .control(purchasePriceField)
+            .value as String? ?? "";
         final price = int.tryParse(purchase);
         return price ?? 0;
       }
     case sellPriceField:
-      return f.control(sellPriceField).value as int? ?? 0;
+      return f
+          .control(sellPriceField)
+          .value as int? ?? 0;
     case quantityField:
-      return f.control(quantityField).value as int? ?? 0;
+      return f
+          .control(quantityField)
+          .value as int? ?? 0;
     case otherCostField:
-      return f.control(otherCostField).value as int? ?? 0;
+      return f
+          .control(otherCostField)
+          .value as int? ?? 0;
   }
   throw Exception("Invalid field: $field");
 }
@@ -61,31 +78,43 @@ bool getBool(AbstractControl<dynamic> form, String field) {
   final f = form as FormGroup;
   switch (field) {
     case useFbaField:
-      return f.control(useFbaField).value as bool;
+      return f
+          .control(useFbaField)
+          .value as bool;
     case autogenSkuField:
-      return f.control(autogenSkuField).value as bool;
+      return f
+          .control(autogenSkuField)
+          .value as bool;
   }
   throw Exception("Invalid field: $field");
 }
 
 PurchaseItemCondition getCondition(AbstractControl<dynamic> form) {
   final f = form as FormGroup;
-  return f.control(conditionField).value as PurchaseItemCondition;
+  return f
+      .control(conditionField)
+      .value as PurchaseItemCondition;
 }
 
 DateTime getPurchaseDate(AbstractControl<dynamic> form) {
   final f = form as FormGroup;
-  return f.control(purchaseDateField).value as DateTime;
+  return f
+      .control(purchaseDateField)
+      .value as DateTime;
 }
 
 String? getListingDateString(AbstractControl<dynamic> form) {
   final f = form as FormGroup;
-  final date = f.control(listingDateField).value as DateTime?;
+  final date = f
+      .control(listingDateField)
+      .value as DateTime?;
   return date == null ? null : currentTimeString(time: date);
 }
 
 List<String> getImages(AbstractControl<dynamic> form) {
   final f = form as FormGroup;
-  final val = f.control(listingImagesField).value as List<String?>;
-  return val.whereNotNull().toList();
+  final val = f
+      .control(listingImagesField)
+      .value as List<String?>;
+  return val.nonNulls.toList();
 }
